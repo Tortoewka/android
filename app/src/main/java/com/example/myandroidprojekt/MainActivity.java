23a6fd14b9595fcbc24b4cefd796b3a65ca7c999
android <https://github.com/Tortoewka/android.git>
2021-02-18 21:31:01 +0500

    lisson1

--- app/src/main/java/com/example/myandroidprojekt/MainActivity.java
+++ app/src/main/java/com/example/myandroidprojekt/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.myandroidprojekt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            EditText editText = findViewById(R.id.editText);
+            TextView textView = findViewById(R.id.textView);
+            textView.setText(editText.getText().toString());
+        });
+
     }
 }
